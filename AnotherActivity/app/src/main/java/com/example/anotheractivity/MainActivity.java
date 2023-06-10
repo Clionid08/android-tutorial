e06fe4baeeceb35cefa41d51313d5df5227fed8d
namuhaja <namuhaja@naver.com>
2023-06-10 09:40:04 +0900

    another Activity name

--- AnotherActivity/app/src/main/java/com/example/anotheractivity/MainActivity.java
+++ AnotherActivity/app/src/main/java/com/example/anotheractivity/MainActivity.java
@@ -6,9 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    int request_Code = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,10 +20,22 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(v.getContext(), SecondActivity.class));
+                startActivityForResult(new Intent(v.getContext(), SecondActivity.class), request_Code);
             }
 
 
         });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode , int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==request_Code) {
+            if(resultCode == RESULT_OK) {
+                Toast.makeText(this, data.getData().toString(), Toast.LENGTH_SHORT).show();
+            }
+        }
+
     }
 }
--- AnotherActivity/app/src/main/java/com/example/anotheractivity/SecondActivity.java
+++ AnotherActivity/app/src/main/java/com/example/anotheractivity/SecondActivity.java
@@ -1,14 +1,34 @@
 package com.example.anotheractivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModel;
 
 public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_second);   
+
+        Button button = (Button) findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent data = new Intent();
+                EditText txt_username=(EditText) findViewById(R.id.txtUsername);
+                data.setData(Uri.parse(txt_username.getText().toString()));
+                setResult(RESULT_OK, data);
+                finish();
+            }
+
+        });
     }
 }
